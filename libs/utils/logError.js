6e5d22ab7236eef71f85f93d69262638f2e9500b
MrRanhkeal <120033960+MrRanhkeal@users.noreply.github.com>
2025-12-05 13:33:17 +0700

    commitd

--- libs/utils/logError.js
+++ libs/utils/logError.js
@@ -1,52 +1,27 @@
+
 const fs = require("fs/promises");
 const path = require("path");
 const moment = require("moment-timezone");
+
+const LOG_DIR = "/var/log/app";   // <-- docker mounted folder
+
 const logError = async (controller, mess_err, res = null) => {
     try {
         const offset = moment().tz("UTC+7").format("YYYY-MM-DD HH:mm:ss z");
         const logDir = path.join(__dirname, "../../logfiles");
-        const logPath = path.join(logDir, `${controller}.tex`);
+        const logPath = path.join(logDir, `${controller}.tex`); 
 
-        // Create logs directory if it doesn't exist
-        await fs.mkdir(logDir, { recursive: true }).catch(() => { });
+        await fs.mkdir(LOG_DIR, { recursive: true }).catch(() => { });
 
         const logMessage = `${mess_err} + ${offset}\n`;
         await fs.appendFile(logPath, logMessage);
-    }
-    catch (error) {
+    } catch (error) {
         console.log("Error writing to log file:", error);
     }
-    
-    if (res && typeof res.status === 'function') {
+
+    if (res && typeof res.status === "function") {
         res.status(500).send("Internal Server Error!");
     }
 };
 
 module.exports = { logError };
-
-// const fs = require("fs/promises");
-// const path = require("path");
-// const moment = require("moment-timezone");
-
-// const LOG_DIR = "/var/log/app";   // <-- docker mounted folder
-
-// const logError = async (controller, mess_err, res = null) => {
-//     try {
-//         const offset = moment().tz("UTC+7").format("YYYY-MM-DD HH:mm:ss z");
-//         const logDir = path.join(__dirname, "../../logfiles");
-//         const logPath = path.join(logDir, `${controller}.tex`); 
-
-//         await fs.mkdir(LOG_DIR, { recursive: true }).catch(() => { });
-
-//         const logMessage = `${mess_err} + ${offset}\n`;
-//         await fs.appendFile(logPath, logMessage);
-//     } catch (error) {
-//         console.log("Error writing to log file:", error);
-//     }
-
-//     if (res && typeof res.status === "function") {
-//         res.status(500).send("Internal Server Error!");
-//     }
-// };
-
-// module.exports = { logError };
